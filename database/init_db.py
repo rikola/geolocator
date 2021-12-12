718ba97207b07894fef7cd915f1170c4071336b7
Eric Koehler <rikola2@gmail.com>
2021-12-11 20:57:36 -0500

    Add new SQL schema and hook it up to views

--- database/init_db.py
+++ database/init_db.py
@@ -1,20 +1,24 @@
 import sqlite3
 
+from database import coordinate_importer
+
+COORDINATE_FILE = 'test_coordinates.csv'
+
 connection = sqlite3.connect('database.db')
 
-
+# Create the table
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
+# Read test data from CSV file.
+locations = coordinate_importer.read_contents(COORDINATE_FILE)
+
+# Insert test data into table.
 cur = connection.cursor()
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
+for loc in locations:
+    cur.execute("INSERT INTO locations (name, latitude, longitude) VALUES (?, ?, ?)",
+                (loc.name, loc.latitude, loc.longitude)
+                )
 
 connection.commit()
 connection.close()
--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS posts;
 
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS locations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL
+    name TEXT NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    description TEXT,
+    category TEXT
 );
